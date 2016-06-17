11f5eebda850e0fd45815ff7b531d5adedb46676
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-06-16 19:04:25 -0700

    Interface for Case and the receipt number

---
+++ USCISTracker/USCISTracker.Data/ICase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USCISTracker.Data
+{
+    public interface ICase
+    {
+
+        /// <summary>
+        /// Case Receipt Number
+        /// </summary>
+        ICaseReceiptNumber ReceiptNumber { get; set; }
+
+        /// <summary>
+        /// Get the form type of this case/ receipt number. I.E : I-765
+        /// </summary>
+        /// <returns></returns>
+        string GetFormType();
+
+        /// <summary>
+        /// Get the last updated time stamp in string. I.E : May 18, 2016
+        /// </summary>
+        /// <returns></returns>
+        string GetLastUpdatedDate();
+
+        /// <summary>
+        /// Get the current status of the case
+        /// </summary>
+        /// <returns></returns>
+        string GetCurrentStatus();
+
+        /// <summary>
+        /// Get the full body message from USCIS
+        /// </summary>
+        /// <returns></returns>
+        string GetFullMessage();
+    }
+}
---
+++ USCISTracker/USCISTracker.Data/ICaseReceiptNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USCISTracker.Data
+{
+    public interface ICaseReceiptNumber
+    {
+        /// <summary>
+        /// Get the actual USCIS Receipt Number
+        /// </summary>
+        /// <returns></returns>
+        string GetReceiptNumber();
+
+        /// <summary>
+        /// Get the name of the Service Center
+        /// </summary>
+        /// <returns></returns>
+        string GetServiceCenter();
+    }
+}
