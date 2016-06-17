6541ed97d0c386283218639c2c7a484ea5b68bff
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-06-16 22:23:23 -0700

    partial code for GenerateFromHTML

--- USCISTracker/USCISTracker.Data/Case.cs
+++ USCISTracker/USCISTracker.Data/Case.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Parser.Html;
+using AngleSharp.Html;
+
 
 namespace USCISTracker.Data
 {
     public class Case : ICase
     {
         #region Fields
-
+        private ICaseReceiptNumber receiptNumber;
+        private string status;
+        private string formType;
+        private string fullMessage;
+        private DateTime lastUpdate;
         #endregion
 
         #region Properties
@@ -17,18 +25,25 @@ namespace USCISTracker.Data
         {
             get
             {
-                throw new NotImplementedException();
+                return receiptNumber;
             }
 
-            set
+            protected set
             {
-                throw new NotImplementedException();
+                receiptNumber = value;
             }
         }
         #endregion
 
 
         #region Constructors
+        /// <summary>
+        /// Default Constructor, Hidden
+        /// </summary>
+        Case()
+        {
+            ReceiptNumber = null;
+        }
 
         #endregion
 
@@ -53,9 +68,28 @@ namespace USCISTracker.Data
             throw new NotImplementedException();
         }
 
-        public static Case GenerateFromHTML(string html)
-        {
 
+        /// <summary>
+        /// Parse the respond HTML and return a new instance of the Case
+        /// </summary>
+        /// <param name="respondHTML">the HTML in string form of the respond page</param>
+        /// <param name="receiptNumber">Receipt number of the responded HTML</param>
+        /// <returns></returns>
+        public static async Task<Case> GenerateFromHTML(string respondHTML, ICaseReceiptNumber receiptNumber)
+        {
+            //Parse the given HTML
+            HtmlParser parser = new HtmlParser();
+            var document = await parser.ParseAsync(respondHTML);
+
+            //Search for the case status
+            var caseStatusDom =  document.All.Where(n => n.ClassName == "current-status-sec").Select(n => n).FirstOrDefault();
+
+            //Search for the mainbody message:
+            var caseMessageDom = document.All.Where(n => n.ClassName == "rows text-center").Select(n => n).FirstOrDefault();
+
+
+            return null;
+            
         }
         #endregion
 
