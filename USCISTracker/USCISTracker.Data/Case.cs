5a33f58d6cfb908ed6fa44e084a1fba942be4707
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-06-16 19:34:25 -0700

    Skeleton code for the Case and CaseReceiptNumber class

---
+++ USCISTracker/USCISTracker.Data/Case.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USCISTracker.Data
+{
+    public class Case : ICase
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+        public ICaseReceiptNumber ReceiptNumber
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #endregion
+
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+        public string GetCurrentStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFormType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFullMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLastUpdatedDate()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+    }
+}
---
+++ USCISTracker/USCISTracker.Data/CaseReceiptNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USCISTracker.Data
+{
+    public class CaseReceiptNumber : ICaseReceiptNumber
+    {
+        #region Fields
+
+        #endregion
+
+
+        #region Properties
+
+        #endregion
+
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        public string GetReceiptNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public string GetServiceCenter()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
