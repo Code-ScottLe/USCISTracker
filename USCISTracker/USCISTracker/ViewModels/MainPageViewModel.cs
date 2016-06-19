00b57315e3685b62e3378211834c161a5ac33652
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-06-19 02:15:34 -0700

    ViewModel Skeleton code

---
+++ USCISTracker/USCISTracker/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USCISTracker.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Helper method to fire the Property Changed
+        /// </summary>
+        /// <param name="propertyName">the name of the changed property</param>
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
