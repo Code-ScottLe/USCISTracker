d1ca37c41ee758fb77e768fb8b47e01a0004a113
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-06-19 22:56:27 -0700

    Applying Master-Detail

--- USCISTracker/USCISTracker/App.xaml.cs
+++ USCISTracker/USCISTracker/App.xaml.cs
@@ -73,7 +73,7 @@ namespace USCISTracker
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(MasterDetailPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- USCISTracker/USCISTracker/ViewModels/MainPageViewModel.cs
+++ USCISTracker/USCISTracker/ViewModels/MainPageViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using USCISTracker.API;
+using USCISTracker.Data;
 
 namespace USCISTracker.ViewModels
 {
@@ -11,10 +14,21 @@ namespace USCISTracker.ViewModels
     {
 
         #region Fields
-
+        private ObservableCollection<ICase> caseCollection;
         #endregion
 
         #region Properties
+
+        /// <summary>
+        /// Collection of all the cases that we are currently tracking
+        /// </summary>
+        public ObservableCollection<ICase> CaseCollection
+        {
+            get
+            {
+                return this.caseCollection;
+            }
+        }
 
         #endregion
 
