7b6550f68fdcd43d08738b5a3c66ae75dd6cf4c2
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-06-20 22:03:49 -0700

    Template10 is in the house

---
+++ USCISTracker/USCISTracker/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using USCISTracker.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace USCISTracker.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
