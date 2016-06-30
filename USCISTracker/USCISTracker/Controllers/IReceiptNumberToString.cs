f28eb542517d0afc6ea0b2b008e8b42a5383ec23
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-06-30 01:27:29 -0700

    make the receipt number to string converter

---
+++ USCISTracker/USCISTracker/Controllers/IReceiptNumberToString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using USCISTracker.Data;
+
+namespace USCISTracker.Controllers
+{
+    public class IReceiptNumberToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            ICaseReceiptNumber receiptNumber = value as ICaseReceiptNumber;
+
+            if (receiptNumber == null)
+            {
+                return null;
+            }
+
+            else
+            {
+                return receiptNumber.ReceiptNumber;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return new CaseReceiptNumber(value as string);
+        }
+    }
+}
