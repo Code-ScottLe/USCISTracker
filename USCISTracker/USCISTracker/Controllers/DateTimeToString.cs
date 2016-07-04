8445c8e0d86d4b44c34c85f3b7b25137553715ff
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-07-04 01:47:21 -0700

    Converter for DateTime and the lastRefresh on the UI

---
+++ USCISTracker/USCISTracker/Controllers/DateTimeToString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace USCISTracker.Controllers
+{
+    public class DateTimeToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(value is DateTime))
+            {
+                throw new InvalidCastException("value is not DateTime");
+            }
+
+            else
+            {
+                return ((DateTime)value).ToString(@"MM\/dd\/yyyy HH:mm");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
