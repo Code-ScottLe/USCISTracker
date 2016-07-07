157525995a7d538f98f31c54fe2f5059e87f056e
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-07-06 19:28:27 -0700

    Finish building the caseCreator

--- USCISTracker/USCISTracker/Views/CaseCreatorContentDialog.xaml.cs
+++ USCISTracker/USCISTracker/Views/CaseCreatorContentDialog.xaml.cs
@@ -19,17 +19,72 @@ namespace USCISTracker.Views
 {
     public sealed partial class CaseCreatorContentDialog : ContentDialog
     {
+        public bool isCancelled = false;
+        private Brush defaultBorderColor;
+
         public CaseCreatorContentDialog()
         {
             this.InitializeComponent();
+            IsPrimaryButtonEnabled = false;
+            defaultBorderColor = ReceiptNumberTextBox.BorderBrush;
         }
 
+        /// <summary>
+        /// Event handler for the Primary Click button of the dialog, which is save in this case.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
+
         }
 
+        /// <summary>
+        /// Event ahandler for the Secondary Click button of the dialog, which is cancel in this case
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
+            isCancelled = true;
+        }
+
+
+        /// <summary>
+        /// Respond to the event that text of the receipt number text box is being changed. This will enable the save button when reached 13 characters.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void ReceiptNumberTextBox_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            if((sender as TextBox).Text.Length == 13)
+            {
+                IsPrimaryButtonEnabled = true;
+            }
+
+            else
+            {
+                IsPrimaryButtonEnabled = false;
+            }
+        }
+
+
+        /// <summary>
+        /// Respond to the vent that text of the receipt number text box has changed. Will warn user if the user mistyped the receipt number
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReceiptNumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if((sender as TextBox).Text.Length != 13)
+            {
+                (sender as TextBox).BorderBrush = new SolidColorBrush() { Color = Windows.UI.Colors.Red };
+            }
+
+            else
+            {
+                (sender as TextBox).BorderBrush = defaultBorderColor;
+            }
         }
     }
 }
