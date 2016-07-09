ea872b5fcf15d715b9332707b51ba5c678e38f0b
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-07-08 18:20:36 -0700

    Build a CaseFactory to generate cases

---
+++ USCISTracker/USCISTracker.Data/CaseFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USCISTracker.Data
+{
+    public static class CaseFactory
+    {
+        /// <summary>
+        /// Generate a case with a given receipt number
+        /// </summary>
+        /// <param name="caseReceiptNumber">Receipt number of the case (Ex: YSC16900xxxxx)</param>
+        /// <returns></returns>
+        public static ICase GetCase(string caseReceiptNumber)
+        {
+            //create a case.
+            Case currentCase = new Case();
+            currentCase.ReceiptNumber = new CaseReceiptNumber(caseReceiptNumber);
+            return currentCase;
+        }
+    }
+}
