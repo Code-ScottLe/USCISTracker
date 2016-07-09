ea8004f524a27745fee408289edb35919f779439
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-07-08 18:22:51 -0700

    Factory now will support case name

--- USCISTracker/USCISTracker.Data/CaseFactory.cs
+++ USCISTracker/USCISTracker.Data/CaseFactory.cs
@@ -13,11 +13,17 @@ namespace USCISTracker.Data
         /// </summary>
         /// <param name="caseReceiptNumber">Receipt number of the case (Ex: YSC16900xxxxx)</param>
         /// <returns></returns>
-        public static ICase GetCase(string caseReceiptNumber)
+        public static ICase GetCase(string caseReceiptNumber, string caseName = "")
         {
             //create a case.
             Case currentCase = new Case();
             currentCase.ReceiptNumber = new CaseReceiptNumber(caseReceiptNumber);
+
+            if(!string.IsNullOrEmpty(caseName))
+            {
+                currentCase.Name = caseName;
+            }
+
             return currentCase;
         }
     }
