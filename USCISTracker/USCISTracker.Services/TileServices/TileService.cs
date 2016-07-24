a26a0bfb1595b974119ffba1406b18cdc9990b85
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-07-24 16:36:13 -0700

    Add in Tile Services

---
+++ USCISTracker/USCISTracker.Services/TileServices/TileService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotificationsExtensions;
+using NotificationsExtensions.Tiles;
+
+namespace USCISTracker.Services.TileServices
+{
+    public class TileService
+    {
+
+
+    }
+}
