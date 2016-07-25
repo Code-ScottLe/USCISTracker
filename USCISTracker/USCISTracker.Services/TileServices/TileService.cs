0e8143551650315b6f3cb270b9863d247beb3e63
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-07-24 17:09:37 -0700

    wrap up tile services

--- USCISTracker/USCISTracker.Services/TileServices/TileService.cs
+++ USCISTracker/USCISTracker.Services/TileServices/TileService.cs
@@ -5,12 +5,80 @@ using System.Text;
 using System.Threading.Tasks;
 using NotificationsExtensions;
 using NotificationsExtensions.Tiles;
+using USCISTracker.Data;
 
 namespace USCISTracker.Services.TileServices
 {
     public class TileService
     {
+        /// <summary>
+        /// Create the main tile notification with the given case.
+        /// </summary>
+        /// <param name="currentCase"></param>
+        /// <returns></returns>
+        public static TileContent CreateAdaptiveMainTileContent(Case currentCase)
+        {
+            //Medium Tile
+            var MediumTile = new TileBinding()
+            {
+                Content = new TileBindingContentAdaptive()
+                {
+                    Children =
+                    {
+                        new AdaptiveText()
+                        {
+                            Text = currentCase.Status,
+                            HintStyle = AdaptiveTextStyle.Caption,
+                            HintWrap = true
+                        }
+                    }
+                },
 
+                DisplayName = currentCase.Name
+            };
+
+            //Wide Tile
+            var WideTile = new TileBinding()
+            {
+                Content = new TileBindingContentAdaptive()
+                {
+                    Children =
+                    {
+                        new AdaptiveText()
+                        {
+                            Text = currentCase.Status,
+                            HintStyle = AdaptiveTextStyle.Body,
+                            HintWrap = true
+                        },
+
+                        new AdaptiveText()
+                        {
+                            Text = $"Refresh on:{currentCase.LastRefresh}",
+                            HintStyle = AdaptiveTextStyle.CaptionSubtle,
+                            HintWrap = true
+                        }
+                    }
+                },
+
+                DisplayName = currentCase.Name
+            };
+
+            //small tile
+
+
+            //Wrap it all together
+            TileContent tileContent = new TileContent()
+            {
+                Visual = new TileVisual()
+                {
+                    TileMedium = MediumTile,
+                    TileWide = WideTile
+                }
+            };
+
+
+            return tileContent;
+        }
 
     }
 }
