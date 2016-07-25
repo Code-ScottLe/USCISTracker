4ed139f7d70852600b343058617fe3e3dfb2b11e
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-07-25 00:28:25 -0700

    Add in the ServiceCompletebackgroundTask to handle app updates

---
+++ USCISTracker/USCISTracker.Background/ServiceCompletedBackgroundTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace USCISTracker.Background
+{
+    public sealed class ServiceCompletedBackgroundTask : IBackgroundTask
+    {
+        BackgroundTaskDeferral deferral;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            deferral = taskInstance.GetDeferral();
+
+            BackgroundExecutionManager.RemoveAccess();
+            await BackgroundExecutionManager.RequestAccessAsync();
+
+            //Write to setting to show as debug value
+            var setting = Windows.Storage.ApplicationData.Current.LocalSettings;
+            setting.Values["ServicingCompleted"] = $"Task ServicingCompleted finished running at {DateTime.Now.ToString()}";
+            System.Diagnostics.Debug.WriteLine($"Task ServicingCompleted finished running at {DateTime.Now.ToString()}");
+
+            deferral.Complete();
+        }
+    }
+}
